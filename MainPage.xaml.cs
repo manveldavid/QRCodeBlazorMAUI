1a74c0dc07e9fa1d9f55f57730da837141aa341d
David Manvel <10kwelldavid@gmail.com>
2024-01-04 23:16:15 +0300

    Добавьте файлы проекта.

---
+++ MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace QRCodeBlazorMAUI
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
